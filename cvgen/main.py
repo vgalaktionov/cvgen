73eca3b8410ec15d6644e43a64a416874b6cef4d
Vadim <vadim.galaktionov@gmail.com>
2019-05-12 21:07:59 +0200

    easier debugging

--- cvgen/main.py
+++ cvgen/main.py
@@ -30,11 +30,16 @@ def load_data(path: PathLike) -> CV:
         raise
 
 
-def render(data: CV, to: PathLike):
+def render(data: CV, to: PathLike, debug: bool = False):
     template = jinja2_env.get_template("index.html.j2")
     rendered = template.render(
         cv=data, timestamp=datetime.datetime.now().strftime("%d-%m-%Y")
     )
+    if debug:
+        click.echo("Rendering template for debugging")
+        with open(CURRENT_DIR / "templates" / "rendered.html", "w") as f:
+            f.write(rendered)
+
     HTML(string=rendered).write_pdf(
         to, stylesheets=[CSS(CURRENT_DIR / "templates" / "style.css")]
     )
@@ -42,13 +47,14 @@ def render(data: CV, to: PathLike):
 
 @click.command()
 @click.argument("input_data")
-def main(input_data: PathLike):
+@click.option("--debug/--no-debug", default=False)
+def main(input_data: PathLike, debug: bool):
     click.echo("CVGen started!")
     cv = load_data(input_data)
     click.echo("Data has been loaded successfully.")
     output_file = Path(f"{cv.name} CV @ {datetime.datetime.now():%d-%m-%Y}.pdf")
 
     click.echo(f'Rendering pdf to "{output_file}" ...')
-    render(cv, output_file)
+    render(cv, output_file, debug)
     click.echo("PDF has rendered.")
     subprocess.Popen(["open", output_file])
